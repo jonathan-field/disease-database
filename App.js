b1f5e8320591f4b1a3b94b2408bfc8b663fe9637
jonathan-field <jonathanfield90@yahoo.com>
2021-10-15 16:28:06 -0400

    Add project to GitHub

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
