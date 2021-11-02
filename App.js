b9443e034c22061d74bf13b0c7d4491d01c18855
jonathan-field <jonathanfield90@yahoo.com>
2021-11-02 14:36:53 -0400

    Refactor and finish LoginScreen

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 import AppLoading from "expo-app-loading";
-import { useFonts, WorkSans_700Bold } from "@expo-google-fonts/work-sans";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import {
+  useFonts,
+  WorkSans_700Bold,
+  WorkSans_400Regular,
+} from "@expo-google-fonts/work-sans";
 import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     WorkSans_700Bold,
+    WorkSans_400Regular,
   });
 
   if (!fontsLoaded) {
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,40 +1,171 @@
 import React from "react";
-import { StyleSheet, View, Text, Image, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import colors from "../config/colors";
 import { LinearGradient } from "expo-linear-gradient";
+import { TextInput, Button } from "react-native-paper";
+import MaskedView from "@react-native-masked-view/masked-view";
+
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 
 export default function LoginScreen(props) {
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={[
-          colors.backgroundtop,
-          colors.backgroundmiddle,
-          colors.backgroundbottom,
-        ]}
-        style={styles.background}
-      />
-      <View style={styles.contentBackground}>
-        <Text style={styles.title}>Disease Database</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          placeholderTextColor="#676767"
+    <DismissKeyboard>
+      <View style={styles.container}>
+        <LinearGradient
+          // Background Linear Gradient
+          colors={[
+            colors.backgroundtop,
+            colors.backgroundmiddle,
+            colors.backgroundbottom,
+          ]}
+          style={styles.background}
         />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#676767"
-        />
+        <View style={styles.contentBackground}>
+          <MaskedView
+            style={{ flex: 1, flexDirection: "row" }}
+            maskElement={
+              <View>
+                <Text style={styles.title}>Disease Database</Text>
+              </View>
+            }
+          >
+            <LinearGradient
+              colors={[
+                colors.backgroundtop,
+                colors.backgroundmiddle,
+                colors.backgroundbottom,
+              ]}
+              start={{ x: 0.6, y: 0 }}
+              end={{ x: 0, y: 0 }}
+              style={{ flex: 1 }}
+            />
+          </MaskedView>
+          <TextInput
+            onPress={Keyboard.dismiss}
+            style={styles.input}
+            mode="outlined"
+            label="Email"
+            theme={{
+              colors: {
+                placeholder: "black",
+                text: "black",
+                primary: "black",
+              },
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            mode="outlined"
+            label="Password"
+            theme={{
+              colors: {
+                placeholder: "black",
+                text: "black",
+                primary: "black",
+              },
+            }}
+          />
+          <Text
+            style={styles.forgotpassword}
+            onPress={() => Alert.alert("Forgot your password pressed")}
+          >
+            Forgot your password?
+          </Text>
+          <Button
+            style={{
+              alignSelf: "center",
+              width: 252.29,
+              marginTop: 20,
+              marginBottom: 6,
+            }}
+            mode="contained"
+            onPress={() => Alert.alert("Log in with Email pressed")}
+            color="#9986F9"
+            dark
+          >
+            <Text style={{ fontFamily: "WorkSans_400Regular" }}>
+              Log in with Email
+            </Text>
+          </Button>
+          <Text style={styles.or}>OR</Text>
+          <Button
+            icon="google"
+            style={styles.button}
+            mode="contained"
+            onPress={() => Alert.alert("Log in with Google pressed")}
+            color="#F5F5F5"
+          >
+            <Text
+              style={{
+                fontFamily: "WorkSans_400Regular",
+                fontSize: 13,
+              }}
+            >
+              Log in with Google
+            </Text>
+          </Button>
+          <Button
+            icon="twitter"
+            style={styles.button}
+            mode="contained"
+            onPress={() => Alert.alert("Log in with Twitter pressed")}
+            color="#1DA1F2"
+            dark
+          >
+            <Text style={{ fontFamily: "WorkSans_400Regular", fontSize: 13 }}>
+              Log in with Twitter
+            </Text>
+          </Button>
+          <Button
+            icon="facebook"
+            style={styles.button}
+            mode="contained"
+            onPress={() => Alert.alert("Log in with Facebook pressed")}
+            color="#1877F2"
+            dark
+          >
+            <Text style={{ fontFamily: "WorkSans_400Regular", fontSize: 13 }}>
+              Log in with Facebook
+            </Text>
+          </Button>
+          <View style={{ marginTop: 10 }}>
+            <Text
+              style={{
+                fontFamily: "WorkSans_400Regular",
+                fontSize: 14,
+                alignSelf: "center",
+              }}
+            >
+              Don't have an account?{" "}
+              <Text
+                style={styles.hyperlinktext}
+                onPress={() => Alert.alert("Sign up pressed")}
+              >
+                Sign up
+              </Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.backgroundImage}>
+          <Image
+            resizeMode="contain"
+            source={require("../assets/loginBackground.png")}
+          />
+        </View>
       </View>
-      <View style={styles.backgroundImage}>
-        <Image
-          resizeMode="contain"
-          source={require("../assets/loginBackground.png")}
-        />
-      </View>
-    </View>
+    </DismissKeyboard>
   );
 }
 
@@ -59,28 +190,45 @@ const styles = StyleSheet.create({
   },
   contentBackground: {
     padding: 15,
-    alignItems: "center",
-    backgroundColor: "#2D428D52",
+    //backgroundColor: "#2D428D52",
+    backgroundColor: "#F5F5F5",
     borderRadius: 20,
-    height: 488,
+    height: 500,
     width: 324,
   },
   title: {
+    alignSelf: "center",
     paddingTop: 20,
     paddingBottom: 20,
     fontSize: 33,
-    fontWeight: "bold",
     fontFamily: "WorkSans_700Bold",
-    color: "#fff",
+    color: "#000000",
   },
   input: {
     height: 40,
     width: 271,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+    margin: 4,
+    alignSelf: "center",
     backgroundColor: "#fff",
-    borderColor: "#fff",
-    borderRadius: 8,
+  },
+  forgotpassword: {
+    left: 15,
+    color: "#616161",
+  },
+  or: {
+    fontFamily: "WorkSans_700Bold",
+    fontSize: 23,
+    margin: 3,
+    textAlign: "center",
+  },
+  button: {
+    width: 252.29,
+    margin: 6,
+    alignSelf: "center",
+    fontFamily: "WorkSans_400Regular",
+  },
+  hyperlinktext: {
+    color: "#0067C6",
+    textDecorationLine: "underline",
   },
 });
