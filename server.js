db6591baee46f70c642cb536ac679a1dcd42d6e3
DisasterMaster3 <harsha.bangaru99@gmail.com>
2021-12-12 15:27:55 -0500

    add instructions to get token

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var request = require('request');
-let searchItem = 'Bacterial pneumonia';
 require("dotenv").config();
 
 var headers = {
     'accept': 'application/json',
     'API-Version': 'v2',
     'Accept-Language': 'en',
-    'Authorization':   `Bearer ${process.env.token}`,
+    'Authorization': `Bearer ${process.env.token}`,
+    // the above token has to be refreshed manual every hour using swagger
+    // input client credentials from https://icd.who.int/icdapi/Account/AccessKey
+    // into swagger https://id.who.int/swagger/index.html
+    // run one of the commands (version: v2, language: en) and get the bearer token from the curl command
+    // throw it into your own .env file
 };
 
-
+let searchItem = 'Bacterial pneumonia';
 var searchDisease = {
     url: `https://id.who.int/icd/entity/search?q=${searchItem}`,
     headers: headers
 };
 
+// see if the disease exists in our database
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         let entity = JSON.parse(body);
@@ -27,14 +32,14 @@ function callback(error, response, body) {
             url: defReq,
             headers: headers
         }
-
+        // let's get the definition of the disease
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(searchItem);
                 let newEnt = JSON.parse(body);
-
+                console.log(body)
                 console.log("\tDefinition: " + newEnt.definition[`@value`])
-                //console.log(body)
+                console.log(body)
             }
             else{
                 console.log("Unable to get definition");
